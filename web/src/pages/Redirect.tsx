b87ac5a15f8be2b0556e42a4070faf694d5bd16a
Grasiela Gomes <produtoradama@gmail.com>
2025-04-22 17:20:40 +0200

    feat: add Redirect component to display redirection message

---
+++ web/src/pages/Redirect.tsx
@@ -0,0 +1,11 @@
+import { Card } from '@/components'
+
+export const Redirect = () => {
+  return (
+    <Card>
+      <p className="text-lg font-semibold mb-4">
+        Você está sendo redirecionado para o link encurtado.
+      </p>
+    </Card>
+  )
+}
