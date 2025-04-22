6d82b601a24522390aece4663d400207ef0ea7d1
Grasiela Gomes <produtoradama@gmail.com>
2025-04-22 17:20:24 +0200

    feat: create MainLayout component for consistent layout structure

---
+++ web/src/layouts/MainLayout.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from 'react-router'
+
+export const MainLayout = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200 p-4">
+      <Outlet />
+    </div>
+  )
+}
