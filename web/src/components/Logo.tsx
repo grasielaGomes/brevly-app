207730f39d36e15af8518da321038d2e9108581b
Grasiela Gomes <produtoradama@gmail.com>
2025-04-23 22:31:41 +0200

    feat: add Logo and LogoIcon components for branding display

---
+++ web/src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import logo from '@/assets/logo.svg'
+import logoIcon from '@/assets/logo-icon.svg'
+
+export const Logo = () => {
+  return <img src={logo} alt="Brev.ly logo" className="w-12 h-12" />
+}
+
+export const LogoIcon = () => {
+  return (
+    <img src={logoIcon} alt="Brev.ly infinite symbol" className="w-12 h-12" />
+  )
+}
