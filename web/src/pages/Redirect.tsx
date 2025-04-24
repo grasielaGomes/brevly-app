4904e00496909e9e2c550b6c90d8d5ab63b3801c
Grasiela Gomes <produtoradama@gmail.com>
2025-04-24 17:10:23 +0200

    refactor: update Redirect component to handle dynamic shortUrl and adjust actionHref

--- web/src/pages/Redirect.tsx
+++ web/src/pages/Redirect.tsx
@@ -1,6 +1,38 @@
+import { FC, useEffect, useCallback } from 'react'
+import { useNavigate, useParams } from 'react-router'
 import { Card, Info, LogoIcon } from '@/components'
+const API = import.meta.env.VITE_API_URL
 
-export const Redirect = () => {
+export const Redirect: FC = () => {
+  const navigate = useNavigate()
+  const { shortUrl } = useParams<{ shortUrl: string }>()
+
+  const handleRedirect = useCallback(async () => {
+    if (!shortUrl) return
+
+    try {
+      const res = await fetch(`${API}/links/${shortUrl}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      })
+
+      if (res.ok) {
+        const data = (await res.json()) as { originalUrl: string }
+        window.location.replace(data.originalUrl)
+      }
+      if (res.status === 404 || res.status === 400) {
+        navigate('/notFound')
+        return
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }, [shortUrl, navigate])
+
+  useEffect(() => {
+    handleRedirect()
+  }, [handleRedirect])
+
   return (
     <Card>
       <Info
@@ -9,7 +41,7 @@ export const Redirect = () => {
         description="O link será aberto automaticamente em alguns instantes.
         Não foi redirecionado?"
         actionText="Acesse aqui"
-        actionHref="https://brev.ly"
+        actionHref="/"
       />
     </Card>
   )
