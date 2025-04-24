8ab6627bf65e9a7f5fd005a2950cc1b6463d5700
Grasiela Gomes <produtoradama@gmail.com>
2025-04-24 17:08:41 +0200

    refactor: enhance Card component to accept optional className prop for styling

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -1,9 +1,13 @@
+import clsx from 'clsx'
 import { FC, PropsWithChildren } from 'react'
 
-export const Card: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <div className="bg-white rounded-lg p-6 w-full max-w-full sm:max-w-[580px] text-center">
-      {children}
-    </div>
+export const Card: FC<PropsWithChildren<{ className?: string }>> = ({
+  children,
+  className,
+}) => {
+  const classes = clsx(
+    'bg-white rounded-lg p-6 text-center w-full max-w-full sm:max-w-[580px]',
+    className
   )
+  return <div className={classes}>{children}</div>
 }
