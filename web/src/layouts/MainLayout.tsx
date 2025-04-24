2c7f34021224e8b876af607822d761c62edd7464
Grasiela Gomes <produtoradama@gmail.com>
2025-04-24 14:56:21 +0200

    fix: adjust padding in MainLayout for improved layout consistency

--- web/src/layouts/MainLayout.tsx
+++ web/src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router'
 
 export const MainLayout = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-gray-200 p-3">
       <Outlet />
     </div>
   )
